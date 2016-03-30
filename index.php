69799ab79a16ecd5fce6f849078640bf09c92b0b
Sven Craehn <crasve15@student.westerdals.no>
2016-03-30 12:31:29 +0200

    Made index buttons with hover effect

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
     <head>
         <meta charset="utf-8">
         <title>Westervalg</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
-    
+        <div id="header">
+            <br>
+            <div id="headLogo"><br><br><br>LOGO</div>
+            <br><br>
+            <hr>
+            <div id="buttons">
+                <div id="utvalgBtn" class="indexBtn">Utvalg</div>
+                <div id="createBtn" class="indexBtn">Hvordan starte utvalg?</div>
+                <div id="aboutBtn" class="indexBtn">About</div>
+            </div>
+        </div>
     </body>
 </html>
