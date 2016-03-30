4f6082a49aace845b0ac741d38cb7e73423ac720
Sven Craehn <crasve15@student.westerdals.no>
2016-03-30 14:14:09 +0200

    Added additional files

---
+++ start.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+       <p>Detta är startsidan.</p>
+        <p>Här ska det stå information om hemsidan osv.</p>
+        <p>Tryck på länkarna!!!!</p>
+    </body>
+</html>
---
+++ utvalg.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+        HELLO WORLD
+        <br>
+        <img width="70%" src="images/student.jpg">
+    </body>
+</html>
