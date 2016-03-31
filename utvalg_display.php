a0c6bbe650e6087e136958ee9e7b890f4c2d1e02
12inchPlasticToy <marcle15@student.westerdals.no>
2016-03-31 20:02:59 +0200

    Added checks handling wrong input and empty results

--- utvalg_display.php
+++ utvalg_display.php
@@ -30,19 +30,35 @@ $sql=
         WHERE kategori IN (".$categories.")
         )";
 
-$stmt = $link->prepare($sql);
+$stmt = $link->prepare(htmlspecialchars($sql));
+
+if($stmt === false){
+    die('<br><br><span style="color:red">
+    Query parameter error.<br>
+    Please check the URL or use the category buttons.</span>');
+}
+
 $stmt->execute();
 
-$stmt->bind_result($utvalg);
+$result = $stmt->get_result();
+
+if($result->num_rows > 0){
 
 /*
 Display the result as a simple table for prototype version
 */
-echo "<table border=\"2\"><tr><th>UTVALG</th></tr>";
-while($stmt->fetch()){
-    echo "<tr><td>".$utvalg."</td></tr>";
+    echo "<table border=\"2\"><tr><th>UTVALG</th></tr>";
+    
+    while($row = $result->fetch_array(MYSQLI_NUM)){
+        echo "<tr><td>".$row[0]."</td></tr>";
+    }
+    
+    echo "</table>";    
 }
-echo "</table>";
+else {
+    echo '<br><br>';
+    echo 'Ingen utvalg tilhører kategorien '.$categories.' for øyeblikket';
+}
 
 $stmt->close();
 
