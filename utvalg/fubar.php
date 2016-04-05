ba906a733a7236244431780a155f5bf4515a51c1
helenethomesen <thohel15@student.westerdals.no>
2016-04-05 16:05:03 +0200

    Created template and first pages for utvalg

---
+++ utvalg/fubar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>#fuBar</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body class="home">
+        <?php include "header.php"; ?>
+    <div id="content" class="faq">
+        <h1>#fuBar</h1>
+        <br>
+        <p>Studentpuben ved Westerdals.</p>
+    </div>
+    </body>
+</html>
---
+++ utvalg/pus.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Utvalg - Prosjektutvalget for spill</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../main.css">
+        <link rel="stylesheet" type="text/css" href="../utvalg_style.css">
+    </head>
+    <body class="home">
+        <?php include "../header.php"; ?>
+    <div id="content" class="utvalg">
+        
+        <div class="logo">
+            <img src="../logos/logo_pus.png" alt="logo">
+        </div>
+        
+    <div id="utvalgContent">
+        <h1>Prosjektutvalget for spill<span id="shortTitle">PUS</span></h1>
+        <p>Sitter du inne med en spillidé, men mangler folk? Mangler teamet ditt en brikke? Vil du bare finne folk å lage spill med? Da er dette utvalget for deg!</p>
+
+        <p>Prosjektutvalget for Spill har som mål å engasjere studenter til å bedrive spillutvikling utenfor klasserommet og på tvers av studieretningene på NITH.
+
+        <p>Dette ved å holde jevnlige møter hvor elever som har idéer, men mangler folk eller ferdigheter, kan presentere disse for andre medstudenter for å finne seg et utviklingsteam.</p>
+
+        <p>Dette kan også være en kanal for allerede etablerte team som kanskje bare mangler en brikke eller en ildsjel som bare vil lage spill.</p>
+
+        <p>Med dette så ønsker vi å åpne for et myldrende spillutviklingsmiljø ved NITH hvor elevene tidlig kan tilegne seg erfaring og bli motivert til å delta i konkurranser og utvide sine porteføljer.</p>
+        
+    </div>
+    <div id="contactBox">
+        <h4>Kontaktinformasjon</h4>
+        <p>Telefonnummer</p>
+        <p>Epost-adresse</p>
+        <p>Webside</p>
+        
+        <div id="tagBox">
+            <h4>Tagger</h4>
+            <p>Interesse, interesse, interesse</p>
+        </div>
+    </div>
+    </div>
+    </body>
+</html>
