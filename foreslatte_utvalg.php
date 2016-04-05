22784bcd76fd05952fb7ebddea6afc489ce81773
Eva Dahlo <daheva15@student.westerdals.no>
2016-04-05 15:32:42 +0200

    Added cards to the foreslatt_utvalg page

--- foreslatte_utvalg.php
+++ foreslatte_utvalg.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Om oss</title>
+        <title>Foreslåtte utvalg</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="main.css">
     </head>
@@ -9,8 +9,58 @@
         <?php include "header.php"; ?>
 
         <div id="content">
-            <p>blablabla</p>
-            <p>Här ska det t.ex. vara en text om oss som administrerar sidan.</p>
+            <p>Her finner du informasjon om foreslåtte utvalg.</p>
+            <p>Hvis det er noen du kunne tenke deg å ta del i kan du gi dem en tommel opp.</p>
+            <div class="visitkortWrapper">
+                <!-- "Thumbs up"-icon used here was made by Freepik from www.flaticon.com, is licensed by Creative Commons BY 3.0 CC 3.0 BY -->
+                <div class="visitkort">
+                    <p>REISEUTVALGET</p>
+                    <hr>
+                    <p>Et utvalg som arrangerer en eller flere reiser innen- eller utenlands i løpet av skoleåret.</p>
+                    <p>Kontaktperson: Kalle Trulsson <br/> Epost: trukal14@student.westerdals.no</p>
+                    <div class = "votes">
+                        <input type="image" src="images/sign.png">
+                        <p>8 personer er interessert i dette utvalget.</p>
+                    </div>
+                </div>
+                <div class="visitkort">
+                    <p>UTVALGET FOR BOKELSKERE</p>
+                    <hr>
+                    <p>Et utvalg som møtes for oss som elsker litteratur.
+                        Vi velger bøker som vi alle leser over en bestemt periode,
+                        slik at vi kan diskutere dem sammen.</p>
+                    <p>Kontaktperson: Anja Alexandra Skog<br/> Epost: skoanj15@student.westerdals.no</p>
+                    <div class = "votes">
+                        <input type="image" src="images/sign.png">
+                        <p>11 personer er interessert i dette utvalget.</p>
+                    </div>
+                </div>
+                <div class="visitkort">
+                    <p>UTVALGET FOR FILM OG FOTO</p>
+                    <hr>
+                    <p>Vi deler tips, bruker hverandre som modeller og statister,
+                        og drar ut sammen for å finne kule steder å ta bilder og film.</p>
+                    <p>Kontaktperson: Maja Nielsen <br/> Epost: niemaj14@student.westerdals.no</p>
+                    <div class = "votes">
+                        <input type="image" src="images/sign.png">
+                        <p>5 personer er interessert i dette utvalget.</p>
+                    </div>
+                </div>
+                <div class="visitkort">
+                    <p>BAKEUTVALGET</p>
+                    <hr>
+                    <p>Et utvalg som baker sammen og deler tips og triks for perfekt bakverk.
+                        Vi kan også samles for å se på serier som "Hele Norge baker" o.l.</p>
+                    <p>Kontaktperson: Gustav Lund <br/> Epost: lungus13@student.westerdals.no</p>
+                    <div class = "votes">
+                        <input type="image" src="images/sign.png">
+                        <p>8 personer er interessert i dette utvalget.</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>"Tommel opp"-ikonet ble laget av <a href="http://www.freepik.com" title="Freepik">Freepik</a> fra <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com.</a> Lisens: <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
+            </div>
         </div>
     </body>
 </html>
