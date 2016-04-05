f726a1908413bd1ba56b1530792700e8ca9f0f45
Clement Marescaux <marcle15@student.westerdals.no>
2016-04-05 13:00:18 +0200

    Add a placeholder for absolute path to content

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
+<?php $path = "";
+//$path = "http://home.nith.no/~marcle15/"
+?>
 <div id="header">
-    <img src="images/CK32.jpg" alt="" id="bg">
+    <img src=<?php echo $path;?>images/CK32.jpg alt="" id="bg">
     <div id="headerTools">
         <span title="Hold Ctrl-tasten nede (Cmd-tasten på Mac). Trykk på + for å forstørre eller - for å forminske."/>
         |<span id="smaller">A </span>
@@ -9,13 +12,13 @@
             <span id="westerdals">Tilbake til Westerdals >></span>
         </a>
     </div>
-    <a href="index.php">
-        <div id="headLogo"><img src="images/Westervalg_stor_logo.png" height="150px"></div>
+    <a href=<?php echo $path;?>index.php>
+        <div id="headLogo"><img src=<?php echo $path;?>images/Westervalg_stor_logo.png height="150px"></div>
         </a>
     <div class="buttons">
-        | <a href="utvalg.php"><div class="indexBtn utvalg" id="utvalgBtn">Utvalg</div></a>
-        | <a href="about.php"><div class="indexBtn about" id="fUtvalgBtn">Foreslåtte utvalg</div></a>
-        | <a href="faq.php"><div class="indexBtn home" id="faqBtn">FAQ</div></a>
+        | <a href=<?php echo $path;?>utvalg.php><div class="indexBtn utvalg" id="utvalgBtn">Utvalg</div></a>
+        | <a href=<?php echo $path;?>about.php><div class="indexBtn about" id="fUtvalgBtn">Foreslåtte utvalg</div></a>
+        | <a href=<?php echo $path;?>faq.php><div class="indexBtn home" id="faqBtn">FAQ</div></a>
         |
     </div>
 </div>
