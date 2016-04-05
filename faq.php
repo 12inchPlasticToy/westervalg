6231cd3ceba1897ebc2afd00ee46b9f2f7d47e71
Eva Dahlo <daheva15@student.westerdals.no>
2016-04-05 11:51:24 +0200

    Updated the text on index.php and faq.php

--- faq.php
+++ faq.php
@@ -10,11 +10,23 @@
     <div id="content" class="faq">
         <h1>FAQ</h1>
         <br>
-        <p>Q: This is a question.</p>
-        <p>A: This is the answer!</p>
+        <p>Q: Jeg vil starte ett nytt utvalg, hvordan går jeg frem?</p>
+        <p>A: Send en epost til oss.</p>
         <hr>
-        <p>Q: This is another question...</p>
-        <p>A: OMG another question what is this even</p>
+        <p>Q: Hva er utvalg?</p>
+        <p>A: Et utvalg er en samling mennesker med en felles interesse som samles for å praktisere denne interessen, f. eks. bowling.</p>
+        <hr>
+        <p>Q: Er ikke utvalg bare for studenter?</p>
+        <p>A: Noen utvalg er kun for studenter, noen kun for ansatte, og resten er for alle som vil delta.</p>
+        <hr>
+        <p>Q: Gjelder denne siden for hele Westerdals?</p>
+        <p>A: Nei, dette er bare for utvalgene på campus Fjerdingen.</p>
+        <hr>
+        <p>Q: Jeg fant en feil i informasjonen om ett av utvalgene, hvordan kan jeg få rettet det opp?</p>
+        <p>A: Send en epost til oss.</p>
+        <hr>
+        <p>Q:Jeg fant en feil på nettsiden deres, hvordan kan jeg informere dere om den?</p>
+        <p>A: Send en epost til oss.</p>
     </div>
     </body>
 </html>
--- index.php
+++ index.php
@@ -8,9 +8,18 @@
     <body>
         <?php include "header.php"; ?>
         <div id="content">
-            <h1>This is index.php</h1>
-            <p>This is the page the header logo direct to.</p>
-            <p>We will be adding info about the page, like what you can do and find here etc, later.</p>
+            <h1>Velkommen til Westervalg!</h1>
+            <p>Her finner du informasjon om utvalgene som finnes ved Campus Fjerdingen,
+                utvalg som noen er interessert i å starte,
+                og informasjon om hvordan man oppretter et utvalg.</p>
+            <p>Et utvalg er på mange måter lik en klubb, og består av ansatte og/eller elever som deler en felles interesse,
+                slik at man kan praktisere den sammen.
+                Utvalg er ment for å skape fellesskap på campuset og gjøre det enklere å bli kjent med andre på tvers av faglige retninger.</p>
+            <ul>
+                <li>FAQ med informasjon om hvordan man oppretter utvalg</li>
+                <li>Utvalg som finnes</li>
+                <li>Foreslåtte utvalg</li>
+            </ul>
         </div>
     </body>
 </html>
