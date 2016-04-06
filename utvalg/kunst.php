1d3d7cf211ee2ec4881dfe85885e41f6360dfee9
helenethomesen <thohel15@student.westerdals.no>
2016-04-06 16:04:39 +0200

    Fixed tag links in a couple of utvalg presentation pages

--- utvalg/kunst.php
+++ utvalg/kunst.php
@@ -12,7 +12,7 @@
         
     <div id="utvalgContent">
         <div class="logo">
-            <img src="../logos/sosialforeningen_logo.png" alt="logo">
+            <img src="../logos/logo_kunst.png" alt="logo">
         </div>
         <h1>Kunstutvalget</h1>
         <p>Er du over gjennomsnittet interessert i kunst og har en indre Picasso som skriker etter å komme ut? Vi i kunstutvalget samles to kvelder i måneden og ser på berømte malerier, historiene rundt disse, reflekterer og gjenskaper vår versjon av kjente kunstverk. Vi deler tips, triks og gir hverandre råd til hvordan å komme i bedre kontakt med vår indre kunstner.</p>
@@ -26,11 +26,13 @@
         <h4>Kontaktinformasjon</h4>
         <p>Telefonnummer: 45393921</p>
         <p>Epost-adresse: lilhan15@student.westerdals.no</p>
-        <p>Webside</p>
+        <p>Webside: www.westerdals.kunstutvalget.no</p>
         
         <div id="tagBox">
-            <h4 id="categoryBox"><br>Kategorier</h4>
+            <h4 id="categoryBox"><br>Kategori</h4>
             <p><a href="../utvalg.php?cat_10=Kunst&show=Vis+valgte">Kunst</a></p>
+            <p><a href="../utvalg.php?cat_6=For+studenter&show=Vis+valgte">For studenter</a></p>
+            <p><a href="../utvalg.php?cat_5=For+ansatte&show=Vis+valgte">For ansatte</a></p>
         </div>
     </div>
     </div>
