05ced2b2e352425c882c0cfaaded00a055e43694
Clement Marescaux <marcle15@student.westerdals.no>
2016-04-07 00:14:29 +0200

    Edited alternative text for logo to a more verbose description

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
             <nav>
                 <ul>
                     <div id="logo">
-                        <img src="<?php echo PATH;?>images/Westervalg_stor_logo.png" alt="logo">
+                        <img src=<?php echo PATH;?>images/Westervalg_stor_logo.png alt="logo">
                     </div>
                     <div id="info">
                         <li><a href="http://www.vg.no">->RAPPORTER FEIL</a></li>
