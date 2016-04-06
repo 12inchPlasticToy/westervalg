ae7d65325d4deddd5f0aa8235d75e2e0dcb120da
Clement Marescaux <marcle15@student.westerdals.no>
2016-04-06 12:01:12 +0200

    Changed absolute path to test all works on server

--- header.php
+++ header.php
@@ -1,18 +1,10 @@
 <!DOCTYPE html>
 <?php 
-/**
-Leave uncommented the corresponding URL:
-- the 127.0.0.1 URL for local servers
-- the home.nith.no URL for the live server
-**/
-
-define("PATH", 
-    "http://127.0.0.1/pj2100-15-gruppe-22/"
-//  "http://home.nith.no/~marcle15/"
-       );
+//$path = "http://127.0.0.1/pj2100-15-gruppe-22/";
+$path = "http://home.nith.no/~marcle15/"
 ?>
 <div id="header">
-    <img src=<?php echo PATH;?>images/CK32.jpg alt="" id="bg">
+    <img src=<?php echo $path;?>images/CK32.jpg alt="" id="bg">
     <div id="headerTools">
         <span title="Hold Ctrl-tasten nede (Cmd-tasten på Mac). Trykk på + for å forstørre eller - for å forminske."/>
         |<span id="smaller">A </span>
@@ -21,13 +13,13 @@ define("PATH",
             <span id="westerdals">Tilbake til Westerdals >></span>
         </a>
     </div>
-    <a href=<?php echo PATH;?>index.php>
-        <div id="headLogo"><img src=<?php echo PATH;?>images/Westervalg_stor_logo.png height="150px"></div>
+    <a href=<?php echo $path;?>index.php>
+        <div id="headLogo"><img src=<?php echo $path;?>images/Westervalg_stor_logo.png height="150px"></div>
         </a>
     <div class="buttons">
-        | <a href=<?php echo PATH;?>utvalg.php><div class="indexBtn utvalg" id="utvalgBtn">Utvalg</div></a>
-        | <a href=<?php echo PATH;?>foreslatte_utvalg.php><div class="indexBtn forUtvalg" id="fUtvalgBtn">Foreslåtte utvalg</div></a>
-        | <a href=<?php echo PATH;?>faq.php><div class="indexBtn home" id="faqBtn">FAQ</div></a>
+        | <a href=<?php echo $path;?>utvalg.php><div class="indexBtn utvalg" id="utvalgBtn">Utvalg</div></a>
+        | <a href=<?php echo $path;?>foreslatte_utvalg.php><div class="indexBtn forUtvalg" id="fUtvalgBtn">Foreslåtte utvalg</div></a>
+        | <a href=<?php echo $path;?>faq.php><div class="indexBtn home" id="faqBtn">FAQ</div></a>
         |
     </div>
 </div>
