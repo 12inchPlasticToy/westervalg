42b6ed8554cf00decf627befd1bcf29a897b321f
Eva Dahlo <daheva15@student.westerdals.no>
2016-04-06 17:06:56 +0200

    Improved FAQ

--- faq.php
+++ faq.php
@@ -10,23 +10,20 @@
         <br>
     <div id="faq">
         <h1>FAQ</h1>
-        <p>Q: Jeg vil starte ett nytt utvalg, hvordan går jeg frem?</p>
-        <p>A: Send en epost med din idé til [XXXX@westervalg.no]. Det eneste vi trenger er navnet på utvalget, en kort beskrivelse av utvalget (max 200 tegn) og din kontaktinformasjon. Vi legger da ut forslaget ditt under <a href="foreslatte_utvalg.php">foreslåtte utvalg</a> for å sjekke hvor stor interesse det finnes for ditt utvalg.</p>
+        <p class="question">Q: Jeg vil starte ett nytt utvalg, hvordan går jeg frem?</p>
+        <p class="answer">A: Send en epost med din idé til [XXXX@westervalg.no]. Det eneste vi trenger er navnet på utvalget, en kort beskrivelse av utvalget (max 200 tegn) og din kontaktinformasjon. Vi legger da ut forslaget ditt under <a href="foreslatte_utvalg.php">foreslåtte utvalg</a> for å sjekke hvor stor interesse det finnes for ditt utvalg.</p>
         <hr>
-        <p>Q: Hva er utvalg?</p>
-        <p>A: Et utvalg er en samling mennesker med en felles interesse som samles for å praktisere denne interessen, f. eks. bowling.</p>
+        <p class="question">Q: Hva er utvalg?</p>
+        <p class="answer">A: Et utvalg er en samling mennesker med en felles interesse som samles for å praktisere denne interessen, f. eks. bowling.</p>
         <hr>
-        <p>Q: Er ikke utvalg bare for studenter?</p>
-        <p>A: Noen utvalg er kun for studenter, noen kun for ansatte, og resten er for alle som vil delta.</p>
+        <p class="question">Q: Er ikke utvalg bare for studenter?</p>
+        <p class="answer">A: Noen utvalg er kun for studenter, noen kun for ansatte, og resten er for alle som vil delta.</p>
         <hr>
-        <p>Q: Gjelder denne siden for hele Westerdals?</p>
-        <p>A: Nei, dette er bare for utvalgene på campus Fjerdingen.</p>
+        <p class="question">Q: Gjelder denne siden for hele Westerdals?</p>
+        <p class="answer">A: Nei, dette er bare for utvalgene på campus Fjerdingen.</p>
         <hr>
-        <p>Q: Jeg fant en feil i informasjonen om ett av utvalgene, hvordan kan jeg få rettet det opp?</p>
-        <p>A: Send en epost til oss på XXXX@westervalg.no. Skriv hvilket utvalg som har feil informasjon og mer spesifikt hva som er feil.</p>
-        <hr>
-        <p>Q:Jeg fant en feil på nettsiden deres, hvordan kan jeg informere dere om den?</p>
-        <p>A: Send en epost til admin på admin@westervalg.no. Fortell hvor du fant feilen og hva som er feil så ordner vi opp i det.</p>
+        <p class="question">Q: Jeg fant en feil på nettsiden deres, hvordan kan jeg informere dere om den?</p>
+        <p class="answer">A: Send en epost til admin på admin@westervalg.no. Fortell hvor du fant feilen og hva som er feil så ordner vi opp i det.</p>
     </div>
     </body>
 </html>
