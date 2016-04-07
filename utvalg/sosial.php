69568f9dce621283e0aed8bfb11b8d7ccea98141
Clement Marescaux <marcle15@student.westerdals.no>
2016-04-07 11:28:55 +0200

    Added footer to an utvalg page - needs debugging

--- utvalg/sosial.php
+++ utvalg/sosial.php
@@ -8,32 +8,32 @@
     </head>
     <body class="home">
         <?php include "../header.php"; ?>
-    <div id="content" class="utvalg">
-        
-    <div id="utvalgContent">
-        <div class="logo">
-            <img src="../logos/logo_sosial.png" alt="sosialutvalgets logo">
+        <div id="content" class="utvalg">
+            
+            <div id="utvalgContent">
+                <div class="logo">
+                    <img src="../logos/logo_sosial.png" alt="sosialutvalgets logo">
+                </div>
+                <h1>Sosialutvalget</h1>
+                <p>Et utvalg for de som er interesserte i å skape et mer sosialt miljø på skolen. Det er vi som står for fester, filmkvelder, sommer og juleavslutninger og andre sosiale aktiviteter.</p>      
+                
+                <p>Hvis du sitter med en idé eller ønsker å bidra med andre planer for et arrangement på skolen, så vil vi i sosialutvalget hjelpe deg med å få arrangementet ditt til å bli en realitet. Er du kreativ, engasjert, festglad og ønsker skape noe stort av ideene, så vet du hvilke utvalg du skal melde deg inn i ;)</p>
+            </div>
+            <div id="contactBox">
+                <h4>Kontaktinformasjon</h4>
+                <p>Telefonnummer: 45129090</p>
+                <p>Epost-adresse: danseb14@westerdals.student.no</p>
+                <p>Webside www.westerdals.sosialutvalget.no</p>
+                
+                <div id="tagBox">
+                    <h4 id="categoryBox"><br>Kategorier</h4>
+                    <p><a href="../utvalg.php?cat_3=Fester&show=Vis+valgte">Fester</a></p>
+                    <p><a href="../utvalg.php?cat_4=Film&show=Vis+valgte">Film</a></p>
+                    <p><a href="../utvalg.php?cat_6=For+studenter&show=Vis+valgte">For studenter</a></p>
+                    <p><a href="../utvalg.php?cat_5=For+ansatte&show=Vis+valgte">For ansatte</a></p>
+                </div>
+            </div>
         </div>
-        <h1>Sosialutvalget</h1>
-        <p>Et utvalg for de som er interesserte i å skape et mer sosialt miljø på skolen. Det er vi som står for fester, filmkvelder, sommer og juleavslutninger og andre sosiale aktiviteter.</p>
-        
-        <p>Hvis du sitter med en idé eller ønsker å bidra med andre planer for et arrangement på skolen, så vil vi i sosialutvalget hjelpe deg med å få arrangementet ditt til å bli en realitet. Er du kreativ, engasjert, festglad og ønsker skape noe stort av ideene, så vet du hvilke utvalg du skal melde deg inn i ;)</p>
-        
-    </div>
-    <div id="contactBox">
-        <h4>Kontaktinformasjon</h4>
-        <p>Telefonnummer: 45129090</p>
-        <p>Epost-adresse: danseb14@westerdals.student.no</p>
-        <p>Webside www.westerdals.sosialutvalget.no</p>
-        
-        <div id="tagBox">
-            <h4 id="categoryBox"><br>Kategorier</h4>
-            <p><a href="../utvalg.php?cat_3=Fester&show=Vis+valgte">Fester</a></p>
-            <p><a href="../utvalg.php?cat_4=Film&show=Vis+valgte">Film</a></p>
-            <p><a href="../utvalg.php?cat_6=For+studenter&show=Vis+valgte">For studenter</a></p>
-            <p><a href="../utvalg.php?cat_5=For+ansatte&show=Vis+valgte">For ansatte</a></p>
-        </div>
-    </div>
-    </div>
+        <?php include "../footer.php"?>
     </body>
 </html>
