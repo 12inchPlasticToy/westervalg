338407134c18af1bd51f9b9dba017cf90a974fce
Sven Craehn <crasve15@student.westerdals.no>
2016-04-07 12:14:05 +0200

    Added the rest of the links in sitemap

--- sitemap.php
+++ sitemap.php
@@ -15,21 +15,21 @@
                     <ul>
                         <a href="utvalg/sosial.php"><li>Sosialutvalget</li></a>
                         <a href="utvalg/pus.php"><li>Prosjektutvalget for spill</li></a>
-                        <a href=""><li>Gründerutvalget</li></a>
+                        <a href="utvalg/grunder.php"><li>Gründerutvalget</li></a>
                         <a href="utvalg/kit.php"><li>Kvinner & IT</li></a>
-                        <a href=""><li>Foreningen for spillkultur</li></a>
+                        <a href="utvalg/ffs.php"><li>Foreningen for spillkultur</li></a>
                         <a href="utvalg/mus.php"><li>Musikkutvalget</li></a>
-                        <a href=""><li>Utviklingsutvalget</li></a>
+                        <a href="utvalg/utvikling.php"><li>Utviklingsutvalget</li></a>
                         <a href="utvalg/fubar.php"><li>Fubar</li></a>
-                        <a href=""><li>Netflix & Chill</li></a>
+                        <a href="utvalg/netflix.php"><li>Netflix & Chill</li></a>
                         <a href="utvalg/poesi.php"><li>Poesiutvalget</li></a>
-                        <a href=""><li>Bowlingutvalget</li></a>
+                        <a href="utvalg/bowling.php"><li>Bowlingutvalget</li></a>
                         <a href="utvalg/friluft.php"><li>Friluftsutvalget</li></a>
                         <a href="utvalg/kunst.php"><li>Kunstutvalget</li></a>
-                        <a href=""><li>Ansattutvalget</li></a>
-                        <a href=""><li>Teater og skuespillutvalget</li></a>
+                        <a href="utvalg/ansatt.php"><li>Ansattutvalget</li></a>
+                        <a href="utvalg/teater.php"><li>Teater og skuespillutvalget</li></a>
                         <a href="utvalg/stayfit.php"><li>StayFitutvalget</li></a>
-                        <a href=""><li>Soultrainutvalget</li></a>
+                        <a href="utvalg/soultrain.php"><li>Soultrainutvalget</li></a>
                     </ul>
                 <a href="foreslatte_utvalg.php"><li>Idéer til utvalg</li></a>
                 <a href="starte_utvalg.php"><li>Starte utvalg</li></a>
