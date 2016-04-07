ab37a96c8371dc8c0c7c3a1149280556e25bd595
Clement Marescaux <marcle15@student.westerdals.no>
2016-04-07 12:17:17 +0200

    Updated utvalg_style for the footer to show properly

--- utvalg/default.php
+++ utvalg/default.php
@@ -10,8 +10,8 @@
     <div id="content">
         <h1>Oops!</h1>
         <br>
-        <p>Websiden du leter etter er ikke satt opp enda</p>
-        <p><a href="../utvalg.php">Trykk her for å komme tilbake til utvalgsøk.</a></p>
+        <p>Websiden du leter etter er ikke satt opp ennå.</p>
+        <p><a href="../utvalg.php">Trykk her for å komme tilbake til utvalgssiden.</a></p>
     </div>
         <?php include "../footer.php"; ?>
     </body>
