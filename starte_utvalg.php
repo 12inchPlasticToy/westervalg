85c69ac477cde3793300b9973ef519ad6f34249a
helenethomesen <thohel15@student.westerdals.no>
2016-04-07 11:22:39 +0200

    Udated start utvalg and added css styling

--- starte_utvalg.php
+++ starte_utvalg.php
@@ -12,8 +12,13 @@
         <h1>Hvordan starte utvalg</h1>
         <h2>Har du en idé til ett nytt utvalg, men vet ikke hvordan du går frem? <br>Send en epost med din idé til starteutvalg@westervalg.no.</h2>
         
-        <p>Det eneste vi trenger er navnet på utvalget, en kort beskrivelse av utvalget (max 200 tegn) og din kontaktinformasjon. Vi legger da ut forslaget ditt under foreslåtte utvalg for å sjekke hvor stor interesse det finnes for ditt utvalg.</p>
-    </div>
+        <p>Det eneste vi trenger er <li>navnet på utvalget</li><li>kort beskrivelse av utvalget (max 200 tegn)</li> <li>din kontaktinformasjon</li><br>.Vi legger da ut forslaget ditt under foreslåtte utvalg for å sjekke hvor stor <br>interesse det finnes for ditt utvalg.</p>
+            
+        <p>Hvis det er nok interesse for din idé trenger du ett styre med minst <b>tre personer</b> som kan ivareta <br>diverse roller. Vanlige roller i ett styre kan være leder, nestleder, økonomiansvarlig eller eventansvarlig. <br>Så må man skrive en søknad hvor man forklarer hva man tilføyer skolen, og  sendte det til <br>studentparlamentet som deretter stemmer over idéen.</p>
+        
+        <img id="visitkortExample" src="images/Starte_utvalg_visitkort.PNG" alt="Eksempel for ditt visitkort">
+        
+        </div>
         <?php include "footer.php"; ?>
     </body>
 </html>
