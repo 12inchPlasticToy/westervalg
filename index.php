d77f24cde05bf3b946adf68cfe65541ee955fac0
Eva Dahlo <daheva15@student.westerdals.no>
2016-04-07 14:54:18 +0200

    Updated the intro text

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
             <p>Her finner du informasjon om utvalgene som finnes ved Campus Fjerdingen,
                 utvalg som noen er interessert i å starte,
                 og informasjon om hvordan man oppretter et utvalg.</p>
-                <br>
+            <p>Vi har inkludert en søkefunksjon på utvalgsiden, 
+                slik at du kan finne utvalg som passer dinne interesser.</p>  
+            <br>
             <img src="images/smilingPeople.jpg" class="homeImages">
             <p>Bilde tatt av <a href="https://www.flickr.com/photos/93963757@N05/" title="Richard Foster">Richard foster</a>, tittel "Closeup portrait of a group of business people laughing", hentet fra <a href="http://www.flickr.com" title="Flickr">www.flickr.com.</a> Lisens: <a href="https://creativecommons.org/licenses/by-sa/2.0/" title="Creative Commons BY 2.0">CC 2.0 BY</a></p>
         </div>
